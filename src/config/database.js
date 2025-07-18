a546f54a9fa6660c0ce299834bfc73ea8cb8dd42
Gsharathchandra <gumasthasharathchandra@gmail.com>
2025-07-18 21:11:37 +0530

    save all

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   
-  await mongoose.connect("mongodb+srv://gumasthasharathchandra:4ZWWWhzFxjSBp5iU@cluster0.qj4eb72.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/devTinder");
+  await mongoose.connect("mongodb+srv://gumasthasharathchandra:4ZWWWhzFxjSBp5iU@cluster0.qj4eb72.mongodb.net/devTinder?retryWrites=true&w=majority&appName=Cluster0");
+
 };
 
 module.exports = connectDB;
