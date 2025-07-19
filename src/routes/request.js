cd4de7c448230b05c19431e43b399591ba52d141
Gsharathchandra <gumasthasharathchandra@gmail.com>
2025-07-19 22:27:39 +0530

    save all

--- src/routes/request.js
+++ src/routes/request.js
@@ -4,7 +4,7 @@ const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 
-requestRouter.post("/request/send/intrested/:toUserId",userAuth, async (req,res)=>{
+requestRouter.post("/request/send/:status/:toUserId",userAuth, async (req,res)=>{
 try {
 const fromUserId = req.user._id;
 const toUserId = req.params.toUserId;
