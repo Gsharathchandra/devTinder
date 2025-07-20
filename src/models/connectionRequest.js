12db097ed41dc04d9d56818725c4aa0a1529d346
Gsharathchandra <gumasthasharathchandra@gmail.com>
2025-07-20 11:42:59 +0530

    compound index

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -19,6 +19,9 @@ const connectionRequestSchema = new mongoose.Schema({
     }
 },{timestamps:true})
 
+
+connectionRequestSchema.index({fromUserId:1,toUserId:1});
+
 // connectionRequestSchema.pre("save",function (){
 //     const connectionRequest = this;
 //     if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
