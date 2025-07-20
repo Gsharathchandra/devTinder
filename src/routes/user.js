3cb4526d5151327d5e2d4f9649375ac32553bb1d
Gsharathchandra <gumasthasharathchandra@gmail.com>
2025-07-20 22:43:31 +0530

    save all

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,4 +1,24 @@
-const express = require("express")
+const express = require("express");
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
 const userRouter = express.Router();
+
+userRouter.get("/user/requests/recieved",userAuth,async (req,res)=>{
+    try {
+        const loggedInUser = req.user;
+    const connectionRequests = await ConnectionRequest.find({
+        toUserId:loggedInUser._id,
+        status:"intrested",
+    })
+    if(!connectionRequests){
+        throw new Error("no requests exists");
+    }
+    res.send(connectionRequests);
+
+    } catch (error) {
+        res.send("error is"+error.message)
+    }
+
+})
 
 module.exports = userRouter;
