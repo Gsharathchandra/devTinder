89a7c760d277ed9c961b2ef2cb074b517e35a88f
Gsharathchandra <gumasthasharathchandra@gmail.com>
2025-07-21 21:55:37 +0530

    save all

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,8 +2,9 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const userRouter = express.Router();
+const User = require("../models/user");
 
-const USER_SAFE_DATA = "firstName lastName age gender about skills photoUrl"
+const USER_SAFE_DATA = "firstName lastName age gender about skills photoUrl";
 
 userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
   try {
@@ -39,22 +40,18 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     const loggedInUser = req.user;
 
     const connectionRequests = await ConnectionRequest.find({
-      $or:[
-        {toUserId: loggedInUser._id},
-        
-        {fromUserId:loggedInUser._id},
-
-     ],
+      $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
       status: "accepted",
-    }).populate("fromUserId",USER_SAFE_DATA ).populate("toUserId",USER_SAFE_DATA );
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
 
-    const data = connectionRequests.map((row)=>{
-        if(row.fromUserId._id.toString() == loggedInUser._id.toString()){
-            return row.toUserId;
-        }
-        else{
-            return row.fromUserId;
-        }
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() == loggedInUser._id.toString()) {
+        return row.toUserId;
+      } else {
+        return row.fromUserId;
+      }
     });
 
     if (!connectionRequests) {
@@ -63,10 +60,42 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 
     res.json({
       message: "data fetched sucessfully",
-      data
+      data,
     });
   } catch (error) {
     res.send("error is" + error.message);
+  }
+});
+
+userRouter.get("/feed", userAuth, async (res, req) => {
+  try {
+    //see all cards except his,his connections,ignord people,already sent request
+    const loggedInUser = req.user;
+
+    const connectionRequests = ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id },
+         { toUserId: loggedInUser._id }],
+    });
+
+    const hideUserFromFeed = new Set();
+
+    connectionRequests.forEach((req)=>{
+        hideUserFromFeed.add(req.fromUserId.toString());
+        hideUserFromFeed.add(req.toUserId.toString());
+    })
+
+    const users = await UserActivation.find({
+        $and:[{ _id:{$nin:Array.from(hideUserFromFeed)}},
+            { _id:{$ne:loggedInUser._id}}
+        ]
+       
+    })
+
+   res.send(users);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
   }
 });
 
