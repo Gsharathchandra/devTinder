7e3466dbf83ce1f37130003ca0c625421e80aca3
Gsharathchandra <gumasthasharathchandra@gmail.com>
2025-07-23 15:16:11 +0530

    save all

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-app.use(express.json());//completed
+app.use(cors());
+app.use(express.json());//completed 
 app.use(cookieParser());
 
 const authRouter = require("./routes/auth");
